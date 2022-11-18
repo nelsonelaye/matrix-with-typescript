89b5041d446c77122c8d70dd235ef795e4ed9ce4
nelsonelaye <nelsonelaye@gmail.com>
2022-11-18 19:42:51 +0100

    init files

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-function create_matrix(str) {
-  const arr = str.split("");
-
-  return arr;
+function create_matrix(str, number_of_column, number_of_row) {
+    var arr = str.split("");
+    var column = number_of_column;
+    var row = number_of_row;
+    // the first for loop handles the number of rows created
+    for (var i = 0; i < row; i++) {
+        // holds new row data
+        var row_data = [];
+        // this second loop handles the number of column created
+        for (var j = 0; j < column; j++) {
+            if (arr[0] == undefined) {
+                // do nothing
+            }
+            else {
+                row_data.push(arr[0]);
+                //remove first element from the array after adding it to the row data
+                arr = arr.filter(function (x) { return x != arr[0]; });
+            }
+        }
+        console.log.apply(console, row_data);
+    }
 }
-
-let input_string = "123456789";
-
-console.log(create_matrix(input_string));
+create_matrix("123456789", 3, 3);
---
+++ index.ts
@@ -0,0 +1,30 @@
+function create_matrix(
+  str: string,
+  number_of_column: number,
+  number_of_row: number
+): any {
+  let arr: (string | number)[] = str.split("");
+  let column: number = number_of_column;
+  let row: number = number_of_row;
+
+  // the first for loop handles the number of rows created
+  for (let i = 0; i < row; i++) {
+    // holds new row data
+    let row_data: (string | number)[] = [];
+
+    // this second loop handles the number of column created
+    for (let j = 0; j < column; j++) {
+      if (arr[0] == undefined) {
+        // do nothing
+      } else {
+        row_data.push(arr[0]);
+
+        //remove first element from the array after adding it to the row data
+        arr = arr.filter((x) => x != arr[0]);
+      }
+    }
+    console.log(...row_data);
+  }
+}
+
+create_matrix("123456789", 3, 3);
